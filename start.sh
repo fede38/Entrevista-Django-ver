876e53f8996b0b213035979059d6b54b0712a1df
Federico Cabral <federicocabral10@hotmail.com>
2021-01-20 17:45:41 -0300

    Cambiado script para que use pip3

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 virtualenv -p python3
 source ./bin/activate
-pip install --upgrade pip
-pip install requirements.txt
+pip3 install --upgrade pip
+pip3 install requirements.txt
 cd todoapp || exit
 python3 manage.py migrate
 python3 manage.py runserver
